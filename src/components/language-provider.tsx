c032fb083242c8b6e65ce5197795b0e322d89a46
Sebastian George <sebastiangeorge004@gmail.com>
2025-08-23 14:16:25 +0000

    make the default language malayalam

--- src/components/language-provider.tsx
+++ src/components/language-provider.tsx
@@ -21,7 +21,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguageState] = useState<Language>('english');
+  const [language, setLanguageState] = useState<Language>('malayalam');
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
